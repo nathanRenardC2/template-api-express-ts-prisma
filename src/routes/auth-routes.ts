f082d4c0d2c71e236bc852aa4d28cae8ca9a7a9a
Nathan Renard <nathan.renard@etu.unif-grenoble-alpes.fr>
2023-04-04 11:54:36 +0200

    authentification finish : we can post an user, post token to have into a json a refresh token and a token, post a refresh token to have a new access token, and get the account of a current user connect

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -33,7 +33,7 @@ router.post(`/register`, async (req: Request, res: Response) => {
  * @param none
  * @body email, password
  */
-router.post(`/login`, async (req: Request, res: Response) => {
+router.post(`/token`, async (req: Request, res: Response) => {
     try {
         const { email, password } = req.body;
 
@@ -77,6 +77,70 @@ router.post(`/login`, async (req: Request, res: Response) => {
     }
 });
 
+/**
+ * Refresh a token
+ * @param none
+ * @body refreshToken
+ */
+router.post(`/refresh_token`, async (req: Request, res: Response) => {
+    try {
+        const { refresh } = req.body;
 
+        const jwtSecret = process.env.JWT_SECRET;
+
+        if (!jwtSecret) {
+            throw new Error("JWT_SECRET is not defined in the environment");
+        }
+
+        const decoded = jwt.verify(refresh, jwtSecret) as { id: string };
+
+        const accessToken = jwt.sign({ id: decoded.id }, jwtSecret, {
+            expiresIn: "1h",
+        });        
+
+        let response_data = {
+            "access": accessToken,
+        }
+
+        res.status(200).json(response_data);
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong", error: error});
+    }
+});
+
+/**
+ * Get the information of the user connected
+ * @param none
+ * @body none
+ */
+router.get(`/account`, async (req: Request, res: Response) => {
+    try {
+        const jwtSecret = process.env.JWT_SECRET;
+
+        if (!jwtSecret) {
+            throw new Error("JWT_SECRET is not defined in the environment");
+        }
+
+        const token = req.headers.authorization?.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const decoded = jwt.verify(token, jwtSecret) as { id: string };
+
+        const user = await prisma.user.findUnique({
+            where: { id: decoded.id },
+        });
+
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong", error: error});
+    }
+});
 
 export const AuthRouter: Router = router;
